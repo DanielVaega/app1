9684c69c561c65cab42b921064580ee333162045
Daniel VP <drifta.dvp@gmail.com>
2023-05-02 18:10:28 +0200

    switched app to MyComponent with title & descr.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { MyComponent } from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MyComponent
+      title={"My App"}
+      description={ "My component's description." }
+    />
   </React.StrictMode>,
 )
