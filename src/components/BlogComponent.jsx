05755dbf7ce0d10c80ed05990962ca462a3c92aa
Daniel VP <drifta.dvp@gmail.com>
2023-05-10 17:20:26 +0200

    blog component changes

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -1,67 +1,67 @@
-import {Link, Outlet, useParams} from "react-router-dom";
-import { NavigationComponent } from "./NavigationComponent";
-export const blogsPosts = [
-    {
-        id: 0,
-    title: "My First Blog",
-    description: "This is my first description.",
-    date: "2020-01-01",
-    images: "https://picsum.photos/id/100/200/300" // *
-},
-    {
-        id: 1,
-    title: "My Second Blog",
-    description: "This is my second description.",
-    date: "2020-01-01",
-    images: "https://picsum.photos/id/100/200/300" // *
-},
-    {
-        id: 2,
-    title: "My Third Blog",
-    description: "This is my third description.",
-    date: "2020-01-01",
-    images: "https://picsum.photos/id/100/200/300" // *
-},
-    {
-        id: 3,
-    title: "My Fourth Blog",
-    description: "This is my fourth description.",
-    date: "2020-01-01",
-    images: "https://picsum.photos/id/100/200/300" // *
-}
-    ]; // pop, shift, push, filter, sort, map,
+import { Link, Outlet, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
 
-export const DisplayBlog = ({ id, title, description, date, image }) => {
+
+export const BlogComponent = () => {
+    const DisplayBlogs = ({ blog }) => {
+        return (
+            <>
+                <div className="blog-post">
+                    <img src={blog.image} alt={blog.headline} />
+                    <h2>{blog.headline}</h2>
+                    <p>{blog.date}</p>
+                    <p>{blog.article}</p>
+                </div>
+            </>
+        );
+    };
+
+    const blogPosts = [
+        {
+            id: 1,
+            headline: 'My First Blog Post',
+            article: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere nulla nec arcu posuere ultrices. Suspendisse potenti. Duis lacinia, risus nec laoreet pellentesque, mauris nibh bibendum mauris, et eleifend est odio ut ipsum.',
+            date: 'May 1, 2022',
+            image: 'https://picsum.photos/200/300'
+        },
+        {
+            id: 2,
+            headline: 'My Second Blog Post',
+            article: 'Praesent ac congue elit. Duis aliquam, enim vel feugiat faucibus, nunc felis porttitor tellus, ac faucibus nisi tellus vel quam. Vestibulum fermentum, nulla vel interdum pellentesque, lorem eros consectetur nisi, in malesuada enim augue sit amet purus.',
+            date: 'June 15, 2022',
+            image: 'https://picsum.photos/200/300'
+        },
+        {
+            id: 3,
+            headline: 'My Third Blog Post',
+            article: 'Curabitur vel sapien posuere, faucibus ante sit amet, aliquet arcu. Duis tincidunt sagittis tellus eu congue. Pellentesque consequat tellus vitae gravida consectetur.',
+            date: 'August 30, 2022',
+            image: 'https://picsum.photos/200/300'
+        }
+    ];
+
+    const [selectedBlog, setSelectedBlog] = useState(null);
+
+    const handleBlogClick = (blog) => {
+        setSelectedBlog(blog);
+    };
+
     return (
-            <Link to={`/blogs/${id}`} className="cover">
-                <img src={image} alt={title} />
-                <h1>{title}</h1>
-                <p>{description}</p>
-                <p>{date}</p>
-            </Link>
+        <>
+            <div className="blog-container">
+                {blogPosts.map((blog) => (
+                    <div key={blog.id} onClick={() => handleBlogClick(blog)}>
+                        <DisplayBlogs blog={blog} />
+                    </div>
+                ))}
+                {selectedBlog && (
+                    <div className="selected-blog">
+                        <DisplayBlogs blog={selectedBlog} />
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
 
-export const BlogComponent = () => {
-    const { id } = useParams();
-    return (
-        <>
-            <NavigationComponent/>
-            <div className="blog-container">
-            {!id ? (
-                <>
-                    <div className="blog-posts">
-                        {blogsPosts.map(DisplayBlog(blog))}
-                            <div key={blog.id}>
-                                {DisplayBlog({...blog, key: blog.id})}
-                            </div>
-                    </div>
-                </>
-            ) : (
-                <Outlet/>
-            )}
-            </div>
-        </>
-        
-    )
-}
+export default BlogComponent;
